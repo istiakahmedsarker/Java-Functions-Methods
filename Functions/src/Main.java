da09bca8fa103cb265ce285bb4df44b819618fca
Istiak AHmed <gamerestiak99@gmail.com>
2023-12-31 11:49:15 +0600

    differentiated between return types

--- Functions/src/Main.java
+++ Functions/src/Main.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        sum();
+//        sum();
+        System.out.println(sum2());
+    }
+//    return the value
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter num1");
+        int num1 = in.nextInt();
+        System.out.println("Enter num2");
+        int num2 = in.nextInt();
+        int ans = num1 + num2;
+        return ans;
     }
     static void sum() {
         Scanner in = new Scanner(System.in);
