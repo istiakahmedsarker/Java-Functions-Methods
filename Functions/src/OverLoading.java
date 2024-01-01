1e80166d978768d30ce2b0f6f95b89779c9c548f
Istiak AHmed <gamerestiak99@gmail.com>
2024-01-01 11:53:26 +0600

    completed overloading of functions

---
+++ Functions/src/OverLoading.java
@@ -0,0 +1,12 @@
+public class OverLoading {
+    public static void main(String[] args) {
+        fun(1);
+        fun("Istiak");
+    }
+    static void fun(int a){
+        System.out.println(a);
+    }
+    static void fun(String name){
+        System.out.println(name);
+    }
+}
