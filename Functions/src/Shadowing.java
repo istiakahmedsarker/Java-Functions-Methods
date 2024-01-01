28a23936aef0681e92fcd6351c887078976454fa
Istiak AHmed <gamerestiak99@gmail.com>
2024-01-01 11:41:13 +0600

    described shadowing

---
+++ Functions/src/Shadowing.java
@@ -0,0 +1,12 @@
+public class Shadowing {
+    static int x = 90;// this will be shadowed at line 8
+    public static void main(String[] args) {
+        System.out.println(x);
+        int x =40;// the class variable at line 4 is shadowed by this.
+        System.out.println(x);// scope will begin when value is initialised
+        fun();
+    }
+    static void fun(){
+        System.out.println(x);
+    }
+}
